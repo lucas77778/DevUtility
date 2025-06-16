a0dcead1d95ad55ed7a76b7e165930936067d1b8
AprilNEA <github@sku.moe>
2025-06-16 19:25:49 +0800

    chore(universal-function-macro): add license header and fix wasm_bindgen path
    - Add copyright and dual license header to universal-function-macro
    - Use fully qualified path for wasm_bindgen attribute

--- crates/universal-function-macro/src/lib.rs
+++ crates/universal-function-macro/src/lib.rs
@@ -1,3 +1,16 @@
+// Copyright (c) 2023-2025, ApriilNEA LLC.
+//
+// Dual licensed under:
+// - GPL-3.0 (open source)
+// - Commercial license (contact us)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// See LICENSE file for details or contact admin@aprilnea.com
+
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, ItemFn};
@@ -13,7 +26,7 @@ pub fn universal_function(_args: TokenStream, input: TokenStream) -> TokenStream
 
     let expanded = quote! {
         #(#attrs)*
-        #[cfg_attr(feature = "web", wasm_bindgen)]
+        #[cfg_attr(feature = "web", wasm_bindgen::wasm_bindgen)]
         #[cfg_attr(feature = "desktop", tauri::command)]
         #vis #sig #block
     };
