b8accc1f5f6432465ab5f7245d21f61230322839
AprilNEA <github@sku.moe>
2025-06-16 19:38:11 +0800

    chore: add copyright and dual license header

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2023-2025, ApriilNEA LLC.
+ *
+ * Dual licensed under:
+ * - GPL-3.0 (open source)
+ * - Commercial license (contact us)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE file for details or contact admin@aprilnea.com
+ */
+
 import * as React from "react"
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
 import { CheckIcon, ChevronRightIcon, CircleIcon } from "lucide-react"
--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -1,4 +1,17 @@
-"use client"
+/**
+ * Copyright (c) 2023-2025, ApriilNEA LLC.
+ *
+ * Dual licensed under:
+ * - GPL-3.0 (open source)
+ * - Commercial license (contact us)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE file for details or contact admin@aprilnea.com
+ */
 
 import * as React from "react"
 import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
